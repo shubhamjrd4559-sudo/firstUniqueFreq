6acc231140071fdd22c10765ae79d684bac1a4ab
Shubham kumar <shubhamjrd4559@gmail.com>
2026-05-03 17:37:07 +0530

    Refactor firstUniqueFreq to use unordered_map
    Refactored firstUniqueFreq function to use unordered_map for frequency counting, improving efficiency. Added comments for clarity.

--- firstUniqueFreq.cpp
+++ firstUniqueFreq.cpp
@@ -1,25 +1,44 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int firstUniqueFreq(vector<int> &nums){
-    vector<int> freq(100001, 0);
+    
+    unordered_map<int, int> freq;
 
+    // count freq of each number
     for(int x : nums){
         freq[x]++;
-    } //2→3, 3→1 ,1→1, 4→1,5→2 ,6→2
+    }
+    // example:
+    // 2 → 3 times  
+    // 3 → 1  
+    // 1 → 1  
+    // 4 → 1  
+    // 5 → 2  
+    // 6 → 2 
 
-    vector<int> freqcnt(100001, 0);
+    unordered_map<int, int> freqCount;
 
-    for(int p : freq){
-        freqcnt[p]++;
-    }//1→3 , 2→2 ,3→1
+    // count how many numbers have a given freq
+    for(auto &p : freq){
+        freqCount[p.second]++;  
+        // p.first  = number
+        // p.second = frequency
+    }
 
+    // freq ka freq
+    // 1 → 3 numbers (3,1,4)
+    // 2 → 2 numbers (5,6)
+    // 3 → 1 number (2) ✅
+
+    // find first number whose freq is unique
     for(int x : nums){
-        if(freqcnt[freq[x]] == 1){
+        if(freqCount[freq[x]] == 1){
             return x;
         }
     }
 
+    // Only frequency 3 unique hai, so answer = 2
     return -1;
 }
 
